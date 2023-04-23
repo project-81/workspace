ce3be17de5ed13b750b2f52327262e038ccaeaf2
Vaughn Kottler <vaughnkottler@gmail.com>
2023-04-23 16:39:03 -0500

    Raspberry Pi bootstrap script updates

--- scripts/raspberrypi/bootstrap.sh
+++ scripts/raspberrypi/bootstrap.sh
@@ -3,7 +3,15 @@
 set -e
 
 # Install git so we can clone the repository.
-sudo apt-get install git -y
+if ! type git >/dev/null; then
+	sudo apt-get install git -y
+fi
+
+# Add github's SSH key so we can clone without a prompt.
+if ! grep -q "github.com" ~/.ssh/known_hosts; then
+	echo "github.com SSH key not found."
+	ssh-keyscan github.com >> ~/.ssh/known_hosts
+fi
 
 # Clone the repository and run the bootstrapping script.
 cd && git clone git@github.com:project-81/workspace.git
