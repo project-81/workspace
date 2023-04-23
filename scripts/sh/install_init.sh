28565c06e6e2a179516e27ae64c0b5c7e29c5441
Vaughn Kottler <vaughnkottler@gmail.com>
2023-04-23 13:56:09 -0500

    Fix ping on WSL

--- scripts/sh/install_init.sh
+++ scripts/sh/install_init.sh
@@ -3,4 +3,8 @@
 if is_wsl; then
 	# https://github.com/tmux/tmux/issues/1092
 	sudo systemd-tmpfiles --create
+
+	# https://github.com/microsoft/WSL/discussions/8484
+	install_package libcap-progs
+	sudo setcap cap_net_raw=p /usr/bin/ping
 fi
