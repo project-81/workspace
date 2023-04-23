df2e4b7970e34020c47da884d019951eca2e4ee2
Vaughn Kottler <vaughnkottler@gmail.com>
2023-04-23 16:16:46 -0500

    Initial bootstrapping implementation

--- scripts/raspberrypi/bootstrap.sh
+++ scripts/raspberrypi/bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get upgrade -y
+
+# Install git so we can clone the repository.
 sudo apt-get install git
+
+# Clone the repository and run the bootstrapping script.
+cd && git clone git@github.com:project-81/workspace.git
+cd ~/workspace && ./scripts/bootstrap.sh
+cd || exit
