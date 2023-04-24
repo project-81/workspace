5148ad5ca5667a56842297820ebc50ddadb6d60a
Vaughn Kottler <vaughnkottler@gmail.com>
2023-04-24 17:13:16 -0500

    Add check for REPO variable

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,10 @@
 set -e
 
+if [ -z "$REPO" ] || [ ! -d "$REPO" ]; then
+	echo "REPO not set to a real directory: '$REPO'!"
+	exit 1
+fi
+
 export SCRIPTS="$REPO/scripts"
 export THIRD_PARTY="$HOME/third-party"
 
