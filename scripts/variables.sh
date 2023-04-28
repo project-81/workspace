5ecf66070f19d76b46ca538f8b17b5201585f3ee
Vaughn Kottler <vaughnkottler@gmail.com>
2023-04-28 03:21:15 -0500

    Fix PYTHON_VERSION stomping

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,10 +1,9 @@
-if [ -z "$PYTHON_VERSION" ]; then
-	if is_rpi; then
-		export PYTHON_VERSION=3.9
-	else
-		export PYTHON_VERSION=3.11
-	fi
+if is_rpi; then
+	export PYTHON_VERSION=3.9
+else
+	export PYTHON_VERSION=3.11
 fi
+
 [ "$GO_VERSION" ] || export GO_VERSION=1.20
 
 VENV="$HOME/venv$PYTHON_VERSION"
