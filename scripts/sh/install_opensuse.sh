6ff19a27a39bfcc6392a76d8ad8e5e861d0fe4ef
Vaughn Kottler <vaughnkottler@gmail.com>
2023-05-04 02:54:09 -0500

    Add 2 dependencies

--- scripts/sh/install_opensuse.sh
+++ scripts/sh/install_opensuse.sh
@@ -6,4 +6,5 @@ assert_opensuse
 TASKS+=(init update git python go zsh fzf-src)
 TASKS+=(neovim rcmpy ninja-src tio rust)
 
-PACKAGES+=(tmux htop rsync psmisc ack mtools graphviz gcc make ShellCheck)
+PACKAGES+=(tmux htop tree rsync psmisc ack mtools graphviz gcc make)
+PACKAGES+=(ShellCheck universal-ctags)
