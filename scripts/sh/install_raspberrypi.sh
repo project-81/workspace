8874f71ce2a74501e6475fa0f0c6a99e1eb4efac
Vaughn Kottler <vaughnkottler@gmail.com>
2023-05-08 13:37:06 -0500

    Small fix for raspberry pi bootstrapping

--- scripts/sh/install_raspberrypi.sh
+++ scripts/sh/install_raspberrypi.sh
@@ -2,10 +2,10 @@
 
 TASKS+=(openocd)
 
-# REPOS=(pico-sdk pico-examples pico-extras pico-playground pico-bootrom)
-# REPOS+=(picotool picoprobe)
-# REPOS+=(openocd picoprobe)
+# REPOS=(pico-examples pico-extras pico-playground)
+REPOS=(pico-sdk pico-bootrom)
+REPOS+=(picotool picoprobe openocd)
 
-# for repo in "${REPOS[@]}"; do
-# 	clone_third_party_github raspberrypi "$repo"
-# done
+for repo in "${REPOS[@]}"; do
+	clone_third_party_github raspberrypi "$repo"
+done
