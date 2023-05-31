eecd0de304d57aeb428ec004c4b08905015208bd
Vaughn Kottler <vaughnkottler@gmail.com>
2023-05-31 02:05:42 -0500

    Add stuff for kicad

---
+++ hooks/kicad_hooks.py
@@ -0,0 +1,48 @@
+"""
+A module for implementing ctags hooks.
+"""
+
+# built-in
+from pathlib import Path
+from typing import Any, Dict
+
+# third-party
+from userfs.build import run_process
+from userfs.config import ProjectSpecification
+from vcorelib.paths.context import in_dir
+
+# internal
+from hooks_common import PREFIX, is_local_bin
+
+
+def post_fetch(
+    root: Path,
+    project: ProjectSpecification,
+    _: Dict[str, Any],
+    __: Dict[str, Any],
+) -> None:
+    """Project interaction."""
+
+    if is_local_bin(project.repository):
+        return
+
+    with in_dir(project.location(root=root)):
+        build = Path("build")
+        release = build.joinpath("release")
+        release.mkdir(exist_ok=True, parents=True)
+        build.joinpath("debug").mkdir(exist_ok=True, parents=True)
+
+        with in_dir(release):
+            if not Path("build.ninja").is_file():
+                run_process(
+                    project.logger,
+                    [
+                        "cmake",
+                        "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
+                        f"-DCMAKE_INSTALL_PREFIX={PREFIX}",
+                        "-DCMAKE_CXX_FLAGS=-fuse-ld=mold",
+                        "../../",
+                    ],
+                )
+            run_process(project.logger, ["ninja"])
+            run_process(project.logger, ["ninja", "install"])
--- scripts/sh/install_debian.sh
+++ scripts/sh/install_debian.sh
@@ -13,7 +13,7 @@ fi
 
 # build tools
 PACKAGES+=(cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential)
-PACKAGES+=(libstdc++-arm-none-eabi-newlib)
+PACKAGES+=(libstdc++-arm-none-eabi-newlib mold)
 
 # gnu
 PACKAGES+=(autoconf automake texinfo flex bison help2man gawk libtool-bin)
@@ -21,8 +21,28 @@ PACKAGES+=(autoconf automake texinfo flex bison help2man gawk libtool-bin)
 # unused: swig
 PACKAGES+=(device-tree-compiler pkgconf libncurses-dev)
 
+
 # gdb and other dev tooling
 PACKAGES+=(gdb-multiarch lcov)
 
 # utilities
 PACKAGES+=(tmux ack htop mtools shellcheck tree)
+
+# stuff for kicad
+if ! is_rpi; then
+	PACKAGES+=(libglu1-mesa-dev libgl1-mesa-dev libglew-dev libglm-dev)
+	PACKAGES+=(libcurl4-openssl-dev libcairo2-dev libboost-all-dev)
+
+	PACKAGES+=(libharfbuzz-dev libngspice0-dev)
+
+	PACKAGES+=(libocct-modeling-algorithms-dev)
+	PACKAGES+=(libocct-modeling-data-dev)
+	PACKAGES+=(libocct-data-exchange-dev)
+	PACKAGES+=(libocct-visualization-dev)
+	PACKAGES+=(libocct-foundation-dev)
+	PACKAGES+=(libocct-ocaf-dev)
+
+	PACKAGES+=(swig4.0 unixodbc-dev)
+
+	PACKAGES+=(libwxgtk3.2-dev libgtk-3-dev)
+fi
