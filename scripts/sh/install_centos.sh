09a7969b1d34f3d1b6799d0fba69bf4710a24c97
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2023-06-23 03:54:15 +0000

    Add perl modules needed for lcov

--- scripts/sh/install_centos.sh
+++ scripts/sh/install_centos.sh
@@ -5,3 +5,7 @@ TASKS+=(git autoconf-src python rcmpy neovim zsh ninja-src)
 
 PACKAGES+=(python$PYTHON_VERSION python$PYTHON_VERSION-pip)
 PACKAGES+=(automake texinfo help2man libtool)
+
+for MOD in Time-HiRes Capture-Tiny Module-Load-Conditional DateTime JSON-XS; do
+	PACKAGES+=(perl-$MOD)
+done
