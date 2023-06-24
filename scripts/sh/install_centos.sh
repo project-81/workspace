354eb5c4a815c232ba5006a2e47950f46e275c42
Vaughn Kottler <vaughnkottler@gmail.com>
2023-06-24 18:25:29 -0500

    Some shell linting fixes

--- scripts/sh/install_centos.sh
+++ scripts/sh/install_centos.sh
@@ -3,9 +3,9 @@
 # tio
 TASKS+=(git autoconf-src python rcmpy neovim zsh ninja-src)
 
-PACKAGES+=(python$PYTHON_VERSION python$PYTHON_VERSION-pip)
+PACKAGES+=("python$PYTHON_VERSION" "python$PYTHON_VERSION-pip")
 PACKAGES+=(automake texinfo help2man libtool)
 
 for MOD in Time-HiRes Capture-Tiny Module-Load-Conditional DateTime JSON-XS; do
-	PACKAGES+=(perl-$MOD)
+	PACKAGES+=("perl-$MOD")
 done
