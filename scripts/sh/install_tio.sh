41c8a3b665caf63d3481c6f537ff49c4cec37d35
Vaughn Kottler <vaughnkottler@gmail.com>
2023-07-22 14:20:13 -0500

    Update tio install script

--- scripts/sh/install_tio.sh
+++ scripts/sh/install_tio.sh
@@ -1,10 +1,14 @@
+PROJECT=tio
+
+if is_local_bin $PROJECT; then
+	return
+fi
+
 if ! is_cmd meson; then
 	run_install python
 fi
 
 meson --version
-
-PROJECT=tio
 
 clone_third_party_github $PROJECT $PROJECT
 safe_pushd "$THIRD_PARTY/$PROJECT"
