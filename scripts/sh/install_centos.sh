0590aa14d328400c8367fd73480b9c89e08eb096
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2023-07-25 14:58:46 -0700

    Add ShellCheck install to CentOS

--- scripts/sh/install_centos.sh
+++ scripts/sh/install_centos.sh
@@ -4,7 +4,7 @@
 TASKS+=(git autoconf-src python rcmpy neovim zsh ninja-src)
 
 PACKAGES+=("python$PYTHON_VERSION" "python$PYTHON_VERSION-pip")
-PACKAGES+=(automake texinfo help2man libtool patch gcc-c++)
+PACKAGES+=(automake texinfo help2man libtool patch gcc-c++ ShellCheck)
 
 for MOD in Time-HiRes Capture-Tiny Module-Load-Conditional DateTime JSON-XS; do
 	PACKAGES+=("perl-$MOD")
