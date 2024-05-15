a9be5324cfa7c783787ee2fb62c3cafbc452a22f
Vaughn Kottler <vaughnkottler@gmail.com>
2024-05-15 12:29:19 -0700

    Try to fix locale-gen script

--- scripts/sh/install_locale.sh
+++ scripts/sh/install_locale.sh
@@ -2,5 +2,10 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US
 
+if is_rpi; then
+	sudo_cmd perl -pi -e \
+		's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+fi
+
 sudo_cmd locale-gen "$LANG"
 sudo_cmd update-locale
